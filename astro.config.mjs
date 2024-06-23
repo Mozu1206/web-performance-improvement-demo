daaa064f392543565b46edb116d7272b4bdbba4e
Masayuki Takahashi <baskemasa1206@gmail.com>
2024-06-23 22:54:52 +0900

    Fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Mozu1206.github.io',
+  base: 'web-performance-improvement-demo'
 });
