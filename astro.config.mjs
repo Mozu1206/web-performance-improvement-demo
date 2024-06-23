1d9ff2def5e0442fe2d30e554c4cef28b1ba6bb8
Masayuki Takahashi <baskemasa1206@gmail.com>
2024-06-23 22:44:52 +0900

    Revert "Update defineConfig"
    This reverts commit e544c7c2625f18d80bdeba2b8a7207cc9bdbcff4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Mozu1206.github.io',
-  base: 'web-performance-improvement-demo'
 });
